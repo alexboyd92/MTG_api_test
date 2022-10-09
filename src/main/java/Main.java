b86ed0e44c7764b62bf2a669ee33d262cf6ce631
MyrTamer <alexanderboyd92@gmail.com>
2022-10-09 12:56:01 -0400

    closed the scanner and writer. Moved the main menu to its own function.. for some reason

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,14 @@ import java.util.List;
 
 public class Main {
 public static void main(String[] args) {
+
+mainMenu();
+System.out.println("Closing");
+}
+public static String searchCard(){
+    return null;
+}
+public static void mainMenu(){
     // Create scanner for user input
     Scanner userInput = new Scanner(System.in);
     //Set default choice
@@ -15,40 +23,35 @@ public static void main(String[] args) {
     // Ask for user input
     do{
         //print menu
-    System.out.printf("Choose an option.\n" +
-            "1) Search for Card\n" +
-            "2) Update card list **Warning will take a while**\n" +
-            "3) Exit\n");
-    // get choice
-    String choiceStr = userInput.nextLine();
-    // attempt to parse integer into option
-    try{
-        //parse string into integer
-       choice= Integer.parseInt(choiceStr);
-       // see if the integer is an option
-       switch (choice){
-          case 1: searchCard();
-          break;
-          case 2: updateList();
-          System.out.println("Card list updated");
-          break;
-          case 3:
-              break;
-           default: System.out.println("Invalid Input");
-           choice=3;
-           break;
-       }
-    }catch (Exception NumberFormatException){
-        choice=0;
-        System.out.printf("Invalid input\n");
-    }} while (choice!=3);
-    //updateList();
+        System.out.printf("Choose an option.\n" +
+                "1) Search for Card\n" +
+                "2) Update card list **Warning will take a while**\n" +
+                "3) Exit\n");
+        // get choice
+        String choiceStr = userInput.nextLine();
+        // attempt to parse integer into option
+        try{
+            //parse string into integer
+            choice= Integer.parseInt(choiceStr);
+            // see if the integer is an option
+            switch (choice){
+                case 1: searchCard();
+                    break;
+                case 2: updateList();
+                    System.out.println("Card list updated");
+                    break;
+                case 3:
+                    break;
+                default: System.out.println("Invalid Input");
+                    choice=3;
+                    break;
+            }
+        }catch (Exception NumberFormatException){
+            choice=0;
+            System.out.printf("Invalid input\n");
+        }} while (choice!=3);
+    userInput.close();
 
-
-
-}
-public static String searchCard(){
-    return null;
 }
 public static void updateList(){
     //create list to store cards and get all cards from the API
@@ -56,12 +59,15 @@ public static void updateList(){
     // Try to write the cards to a file
     try {
         FileWriter writer = new FileWriter("src/main/resources/card_List.txt");
+        //Write each card in the list
         for(Card str: cards) {
             writer.write(str + System.lineSeparator());
         }
+        writer.close();
     } catch(Exception ex) {
         // print stack trace if failed
         ex.printStackTrace();
     }
+
 }
 }
